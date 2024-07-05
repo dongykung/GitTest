849fab6f9336a33330d2bbe9bef20bdf46791a93
kim dong kyung <kdkd422@naver.com>
2024-07-05 21:43:37 +0900

    First Commit

---
+++ GitTest/GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by 김동경 on 2024/07/05.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
